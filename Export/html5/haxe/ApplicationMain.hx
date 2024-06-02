41230d8855a4572ad60e357b2fe120bb441f989e
SwazLord <swaranzi@gmail.com>
2024-06-03 01:39:56 +0530

    initial commit
    renamed source directory to src

--- Export/html5/haxe/ApplicationMain.hx
+++ Export/html5/haxe/ApplicationMain.hx
@@ -35,7 +35,7 @@ class ApplicationMain
 		ManifestResources.init(config);
 		#end
 
-		app.meta["build"] = "31";
+		app.meta["build"] = "1";
 		app.meta["company"] = "Company Name";
 		app.meta["file"] = "StarlingbuilderHaxe";
 		app.meta["name"] = "StarlingbuilderHaxe";
